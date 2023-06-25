f7f4c4a0d9355df52fdf44bb250e8560f8a92762
QuinnP2910 <72775766+QuinnP2910@users.noreply.github.com>
2023-06-25 10:46:54 -0700

    Make code prettier

--- public/js/front.js
+++ public/js/front.js
@@ -1,12 +1,12 @@
-const canvas = document.getElementById('img-canvas');
-const context = canvas.getContext('2d');
-const slider = document.getElementById('slider');
-const sliderText = document.getElementById('slider-width');
-const output = document.getElementById('ascii-output');
-const canvasCol = document.getElementById('canvas-col');
-const canvasColDiv = document.getElementById('canvas-col-div');
+const canvas = document.getElementById("img-canvas");
+const context = canvas.getContext("2d");
+const slider = document.getElementById("slider");
+const sliderText = document.getElementById("slider-width");
+const output = document.getElementById("ascii-output");
+const canvasCol = document.getElementById("canvas-col");
+const canvasColDiv = document.getElementById("canvas-col-div");
 let image = new Image();
-let imageSource = '#';
+let imageSource = "#";
 
 let horizontalGlyphs = 0;
 let verticalGlyphs = 0;
@@ -15,25 +15,30 @@ let verticalGlyphSkipRatio = 1;
 
 let imageScale = 20;
 
-var drawableCanvas = document.getElementById('drawable-canvas');
+var drawableCanvas = document.getElementById("drawable-canvas");
 document.body.style.margin = 0;
-drawableCanvas.style.position = 'fixed';
+drawableCanvas.style.position = "fixed";
 
-var drawableContext = drawableCanvas.getContext('2d');
+var drawableContext = drawableCanvas.getContext("2d");
 
 var pos = { x: 0, y: 0 };
-window.addEventListener('resize', resize);
-document.addEventListener('mousemove', draw);
-document.addEventListener('mousedown', setPosition);
-document.addEventListener('mouseenter', setPosition);
+window.addEventListener("resize", resize);
+document.addEventListener("mousemove", draw);
+document.addEventListener("mousedown", setPosition);
+document.addEventListener("mouseenter", setPosition);
 
 function resize() {
     drawableCanvas.width = parseFloat(getComputedStyle(canvasCol).width) * 0.7;
     drawableCanvas.height = drawableCanvas.width;
     console.log(canvasCol.getBoundingClientRect().left);
-    drawableCanvas.style.left = (canvasCol.getBoundingClientRect().left + ((canvasCol.getBoundingClientRect().right - canvasCol.getBoundingClientRect().left) * 0.15)) + "px";
-    canvasColDiv.style.height = (drawableCanvas.height + 50) + "px";
-    drawableContext = drawableCanvas.getContext('2d');
+    drawableCanvas.style.left =
+        canvasCol.getBoundingClientRect().left +
+        (canvasCol.getBoundingClientRect().right -
+            canvasCol.getBoundingClientRect().left) *
+            0.15 +
+        "px";
+    canvasColDiv.style.height = drawableCanvas.height + 50 + "px";
+    drawableContext = drawableCanvas.getContext("2d");
     drawableContext.fillStyle = "white";
     drawableContext.fillRect(0, 0, drawableCanvas.width, drawableCanvas.height);
 }
@@ -45,51 +50,59 @@ function setPosition(e) {
 }
 
 function draw(e) {
-  if (e.buttons !== 1) return;
+    if (e.buttons !== 1) return;
 
-  drawableContext.beginPath();
+    drawableContext.beginPath();
 
-  drawableContext.lineWidth = 25;
-  drawableContext.lineCap = 'round';
-  drawableContext.strokeStyle = 'black';
+    drawableContext.lineWidth = 25;
+    drawableContext.lineCap = "round";
+    drawableContext.strokeStyle = "black";
 
-  drawableContext.moveTo(pos.x, pos.y);
-  setPosition(e);
-  drawableContext.lineTo(pos.x, pos.y);
+    drawableContext.moveTo(pos.x, pos.y);
+    setPosition(e);
+    drawableContext.lineTo(pos.x, pos.y);
 
-  drawableContext.stroke();
+    drawableContext.stroke();
 }
 
-window.addEventListener('load', function() {
+window.addEventListener("load", function () {
     resize();
     image.src = imageSource;
-    document.querySelector('input[type="file"]').addEventListener('change', function() {
-        if (this.files && this.files[0]) {
-            image = document.querySelector('img');
-  
-            image.src = URL.createObjectURL(this.files[0])
-            imageSource = image.src;
-        }
-    });
+    document
+        .querySelector('input[type="file"]')
+        .addEventListener("change", function () {
+            if (this.files && this.files[0]) {
+                image = document.querySelector("img");
+
+                image.src = URL.createObjectURL(this.files[0]);
+                imageSource = image.src;
+            }
+        });
 });
 
-slider.oninput = function() {
+slider.oninput = function () {
     imageScale = this.value;
     sliderText.innerText = imageScale + " characters wide";
-}
+};
 
 function makeBlackAndWhite() {
-    image = document.querySelector('img');
-    imageSource = image.getAttribute('src');
+    image = document.querySelector("img");
+    imageSource = image.getAttribute("src");
     image = scaleImage(imageScale, image);
-    image.onload = function(){
+    image.onload = function () {
         canvas.width = image.width;
         canvas.height = image.height;
         context.drawImage(image, 0, 0);
-        const scannedImage = context.getImageData(0, 0, canvas.width, canvas.height);
+        const scannedImage = context.getImageData(
+            0,
+            0,
+            canvas.width,
+            canvas.height
+        );
         const scannedData = scannedImage.data;
         for (let i = 0; i < scannedData.length; i += 4) {
-            const averageColorValue = (scannedData[i] + scannedData[i + 1] + scannedData[i + 2]) / 3
+            const averageColorValue =
+                (scannedData[i] + scannedData[i + 1] + scannedData[i + 2]) / 3;
             // Make fully black and white
             let newColorValue = 0;
             if (averageColorValue > 127) {
@@ -101,53 +114,60 @@ function makeBlackAndWhite() {
         }
         scannedImage.data = scannedData;
         context.putImageData(scannedImage, 0, 0);
-    
+
         const colorProfiles = [];
         for (let i = 0; i < verticalGlyphs; i++) {
             for (let j = 0; j < horizontalGlyphs; j++) {
-                let cell = analyzeCell(scannedData, j, i)
+                let cell = analyzeCell(scannedData, j, i);
                 colorProfiles.push(cell);
                 // scannedImage.data = putCell(cell, scannedData, j, i);
                 // context.putImageData(scannedImage, 0, 0);
             }
         }
-    
+
         const imageInfo = {
             colorProfiles: colorProfiles,
             horizontalGlyphs: horizontalGlyphs,
             verticalGlyphs: verticalGlyphs,
             horizontalGlyphSkipRatio: horizontalGlyphSkipRatio,
             verticalGlyphSkipRatio: verticalGlyphSkipRatio,
-            imageSource: imageSource
+            imageSource: imageSource,
         };
 
-        output.setAttribute('rows', 5);
-        output.setAttribute('cols', 20);
-        output.innerHTML = 'Generating...';
-        fetch('/', {
-            method: 'POST',
+        output.setAttribute("rows", 5);
+        output.setAttribute("cols", 20);
+        output.innerHTML = "Generating...";
+        fetch("/", {
+            method: "POST",
             headers: {
-              'Content-Type': 'application/json'
+                "Content-Type": "application/json",
             },
-            body: JSON.stringify({imageInfo})
+            body: JSON.stringify({ imageInfo }),
         });
-        window.location.href = '/';
-    }
+        window.location.href = "/";
+    };
 }
 
 function processDrawableCanvas() {
     let drawnImage = new Image();
     drawnImage.src = drawableCanvas.toDataURL();
-    drawnImage.onload = function(){
+    drawnImage.onload = function () {
         let scaledDrawnImage = scaleImage(imageScale, drawnImage);
-        scaledDrawnImage.onload = function(){
+        scaledDrawnImage.onload = function () {
             canvas.width = scaledDrawnImage.width;
             canvas.height = scaledDrawnImage.height;
             context.drawImage(scaledDrawnImage, 0, 0);
-            const scannedImage = context.getImageData(0, 0, canvas.width, canvas.height);
+            const scannedImage = context.getImageData(
+                0,
+                0,
+                canvas.width,
+                canvas.height
+            );
             const scannedData = scannedImage.data;
             for (let i = 0; i < scannedData.length; i += 4) {
-                const averageColorValue = (scannedData[i] + scannedData[i + 1] + scannedData[i + 2]) / 3
+                const averageColorValue =
+                    (scannedData[i] + scannedData[i + 1] + scannedData[i + 2]) /
+                    3;
                 // Make fully black and white
                 let newColorValue = 0;
                 if (averageColorValue > 127) {
@@ -159,47 +179,47 @@ function processDrawableCanvas() {
             }
             scannedImage.data = scannedData;
             context.putImageData(scannedImage, 0, 0);
-        
+
             const colorProfiles = [];
             for (let i = 0; i < verticalGlyphs; i++) {
                 for (let j = 0; j < horizontalGlyphs; j++) {
-                    let cell = analyzeCell(scannedData, j, i)
+                    let cell = analyzeCell(scannedData, j, i);
                     colorProfiles.push(cell);
                     // scannedImage.data = putCell(cell, scannedData, j, i);
                     // context.putImageData(scannedImage, 0, 0);
                 }
             }
-        
+
             const imageInfo = {
                 colorProfiles: colorProfiles,
                 horizontalGlyphs: horizontalGlyphs,
                 verticalGlyphs: verticalGlyphs,
                 horizontalGlyphSkipRatio: horizontalGlyphSkipRatio,
                 verticalGlyphSkipRatio: verticalGlyphSkipRatio,
-                imageSource: imageSource
+                imageSource: imageSource,
             };
-    
-            output.setAttribute('rows', 5);
-            output.setAttribute('cols', 20);
-            output.innerHTML = 'Generating...';
-            fetch('/', {
-                method: 'POST',
+
+            output.setAttribute("rows", 5);
+            output.setAttribute("cols", 20);
+            output.innerHTML = "Generating...";
+            fetch("/", {
+                method: "POST",
                 headers: {
-                  'Content-Type': 'application/json'
+                    "Content-Type": "application/json",
                 },
-                body: JSON.stringify({imageInfo})
+                body: JSON.stringify({ imageInfo }),
             });
-            window.location.href = '/';
-        }
-    }
+            window.location.href = "/";
+        };
+    };
 }
 
 function scaleImage(numGlyphs, image) {
     console.log("Image height: " + image.height);
     console.log("Image width: " + image.width);
     const ratio = image.height / image.width;
-    const tempCanvas = document.createElement('canvas');
-    const tempContext = tempCanvas.getContext('2d');
+    const tempCanvas = document.createElement("canvas");
+    const tempContext = tempCanvas.getContext("2d");
 
     let originalWidth = image.width;
 
@@ -208,31 +228,53 @@ function scaleImage(numGlyphs, image) {
     verticalGlyphs = Math.round(scaledHeight / 312);
     let calculatedHeight = verticalGlyphs * 312;
 
-    horizontalGlyphSkipRatio = roundToNearestWidthFactor((horizontalGlyphs * 128) / originalWidth);
-    tempCanvas.width = Math.floor((1 / horizontalGlyphSkipRatio) * (horizontalGlyphs * 128));
-    verticalGlyphSkipRatio = roundToNearestHeightFactor((verticalGlyphs * 312) / ((1 / horizontalGlyphSkipRatio) * calculatedHeight));
-    tempCanvas.height = Math.floor((1 / verticalGlyphSkipRatio) * (verticalGlyphs * 312));
+    horizontalGlyphSkipRatio = roundToNearestWidthFactor(
+        (horizontalGlyphs * 128) / originalWidth
+    );
+    tempCanvas.width = Math.floor(
+        (1 / horizontalGlyphSkipRatio) * (horizontalGlyphs * 128)
+    );
+    verticalGlyphSkipRatio = roundToNearestHeightFactor(
+        (verticalGlyphs * 312) /
+            ((1 / horizontalGlyphSkipRatio) * calculatedHeight)
+    );
+    tempCanvas.height = Math.floor(
+        (1 / verticalGlyphSkipRatio) * (verticalGlyphs * 312)
+    );
 
-    console.log('Horizontal resolution: ' + tempCanvas.width);
-    console.log('Vertical resolution: ' + tempCanvas.height);
+    console.log("Horizontal resolution: " + tempCanvas.width);
+    console.log("Vertical resolution: " + tempCanvas.height);
 
     tempContext.drawImage(image, 0, 0, tempCanvas.width, tempCanvas.height);
 
     const scaledImage = new Image();
     scaledImage.src = tempCanvas.toDataURL();
 
-    console.log('Vertical glyphs: ' + verticalGlyphs);
+    console.log("Vertical glyphs: " + verticalGlyphs);
 
-    console.log('horizontalGlyphSkipRatio: ' + horizontalGlyphSkipRatio);
-    console.log('verticalGlyphSkipRatio: ' + verticalGlyphSkipRatio);
+    console.log("horizontalGlyphSkipRatio: " + horizontalGlyphSkipRatio);
+    console.log("verticalGlyphSkipRatio: " + verticalGlyphSkipRatio);
     return scaledImage;
 }
 
-function analyzeCell(scannedData, horizontalCell, verticalCell){
+function analyzeCell(scannedData, horizontalCell, verticalCell) {
     const colorProfile = new Array();
     for (let i = 0; i < Math.round(312 / verticalGlyphSkipRatio); i += 1) {
-        for (let j = 0; j < Math.round(128 / horizontalGlyphSkipRatio); j += 1) {
-            position = Math.round((i * image.width * 4) + (j * 4) + (horizontalCell * (128 / horizontalGlyphSkipRatio) * 4) + (verticalCell * horizontalGlyphs * (312 / verticalGlyphSkipRatio) * (128 / horizontalGlyphSkipRatio) * 4))
+        for (
+            let j = 0;
+            j < Math.round(128 / horizontalGlyphSkipRatio);
+            j += 1
+        ) {
+            position = Math.round(
+                i * image.width * 4 +
+                    j * 4 +
+                    horizontalCell * (128 / horizontalGlyphSkipRatio) * 4 +
+                    verticalCell *
+                        horizontalGlyphs *
+                        (312 / verticalGlyphSkipRatio) *
+                        (128 / horizontalGlyphSkipRatio) *
+                        4
+            );
             if ((position + 1) % 4 === 0) {
                 position++;
             }
@@ -242,11 +284,26 @@ function analyzeCell(scannedData, horizontalCell, verticalCell){
     return colorProfile;
 }
 
-function putCell(colorProfile, scannedData, horizontalCell, verticalCell){
+function putCell(colorProfile, scannedData, horizontalCell, verticalCell) {
     for (let i = 0; i < Math.round(312 / verticalGlyphSkipRatio); i += 1) {
-        for (let j = 0; j  < Math.round(128 / horizontalGlyphSkipRatio); j += 1) {
-            let color = colorProfile[i * Math.round(128 / horizontalGlyphSkipRatio) + j];
-            let position = Math.round((i * image.width * 4) + (j * 4) + (horizontalCell * (128 / horizontalGlyphSkipRatio) * 4) + (verticalCell * image.width * (312 / verticalGlyphSkipRatio) * 4));
+        for (
+            let j = 0;
+            j < Math.round(128 / horizontalGlyphSkipRatio);
+            j += 1
+        ) {
+            let color =
+                colorProfile[
+                    i * Math.round(128 / horizontalGlyphSkipRatio) + j
+                ];
+            let position = Math.round(
+                i * image.width * 4 +
+                    j * 4 +
+                    horizontalCell * (128 / horizontalGlyphSkipRatio) * 4 +
+                    verticalCell *
+                        image.width *
+                        (312 / verticalGlyphSkipRatio) *
+                        4
+            );
             while (position % 4 !== 0) {
                 position++;
             }
@@ -264,7 +321,7 @@ function roundToNearestWidthFactor(number) {
     let factors = [1, 2, 4, 8, 16];
     let lowestDistance = Number.MAX_SAFE_INTEGER;
     let lowestDistanceFactor = 1;
-    factors.forEach(factor => {
+    factors.forEach((factor) => {
         let distance = Math.abs(number - factor);
         if (distance < lowestDistance) {
             lowestDistance = distance;
@@ -279,7 +336,7 @@ function roundToNearestHeightFactor(number) {
     let factors = [1, 2, 3, 4, 6, 8, 12, 13];
     let lowestDistance = Number.MAX_SAFE_INTEGER;
     let lowestDistanceFactor = 1;
-    factors.forEach(factor => {
+    factors.forEach((factor) => {
         let distance = Math.abs(number - factor);
         if (distance < lowestDistance) {
             lowestDistance = distance;
